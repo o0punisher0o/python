871c2b0021cc811c3850835411a94f3ef0a22965
Akhmetov Kalizhan <100195237+o0punisher0o@users.noreply.github.com>
2022-10-19 17:06:04 +0600

    Create assignment_3.py
    assignnment_3 done

---
+++ week_6_assignment_3/assignment_3.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import requests
+app = Flask(__name__)
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    if request.method == 'POST':
+        address=request.form.get('address')
+        url = 'https://solana-gateway.moralis.io/nft/mainnet/' + address + '/metadata'
+        headers = {
+            "accept": "application/json",
+            "X-API-Key": "iWXzBLaUXSfgBOJ5y8lmb9h5xAstww6nm2wkDTXOxsL1vLeoANc8njHGpnTrQWcM"
+        }
+        response = requests.get(url, headers=headers)
+        print(response.text)
+        return '''
+                  <h1>The information about nft: {}</h1>'''.format(response.text)
+    return '''
+           <form method="POST">
+               <div><label>address: <input type="text" name="address"></label></div>
+               <input type="submit" value="Get Info">
+           </form>'''
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
